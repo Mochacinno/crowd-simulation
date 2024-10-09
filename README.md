6a2b44c052ec1de8162241bccaee5ef6cec9e405
MochinO_O <ha.lecardinal@gmail.com>
2024-10-09 11:30:40 +0200

    Base pour simulation

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 first file
+
+# Consigne
+les N perssones 
+tout le monde va en meme vitesse
--- import pygame.py
+++ import pygame.py
@@ -1,1 +1,27 @@
 import pygame
+import sys
+
+pygame.init()
+
+screen_width, screen_height = 800, 600
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Mvt de Foule")
+
+screen.fill((0,0,0))
+
+class Humain:
+    def __init__(self) -> None:
+        pass
+
+    def appartient_qqn(self):
+        pass
+
+    def move(self):
+        pass
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
