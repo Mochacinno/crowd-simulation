97defed2b4c57be1c37043472babca3114293184
tom-grm88 <tom.germain88@gmail.com>
2024-11-13 11:38:00 +0100

    Fonctionnel sans cible_en_vue

--- import pygame.py
+++ import pygame.py
@@ -36,12 +36,12 @@ class Humain:
     def choisir_cible(self, dict_humains):
         target_ids = [key for key in dict_humains if key != self.id+1]
 
-        index_cible_1, index_cible_2 = np.random.choice(target_ids, 2, replace=False)
-        self.cible1 = dict_humains_prec[index_cible_1]
-        self.cible2 = dict_humains_prec[index_cible_2]
+        index_cible1, index_cible2 = np.random.choice(target_ids, 2, replace=False)
+        self.cible1 = dict_humains[index_cible1]
+        self.cible2 = dict_humains[index_cible2]
     def court_chemin_vect(self):
-        self.pos_percue_cible1 = dict_humains_prec[self.cible_1][1]
-        self.pos_percue_cible2 = dict_humains_prec[self.cible_2][1]
+        self.pos_percue_cible1 = self.cible1.pos
+        self.pos_percue_cible2 = self.cible2.pos
         x1, y1 = self.pos_percue_cible1
         x2, y2 = self.pos_percue_cible2
         vectdir = np.array([x1-x2, y1-y2])
@@ -56,13 +56,13 @@ class Humain:
         # tous les gens bougent en meme temps au lieu que humain1 bouge, qui donc modifie la position pour qqn qui a le cible de humain1
         vect_dir = self.court_chemin_vect() - self.pos
         self.pos = self.pos + vect_dir / np.linalg.norm(vect_dir)
-        dict_humains_suiv[self.id] = self.pos
+        dict_pos_suiv[self.id] = self.pos
     
     def afficher(self, highlight=False):
         if highlight:
             pygame.draw.circle(screen, (255, 0, 0), self.court_chemin_vect(), 2)
-            pygame.draw.line(screen, (0, 0, 255), (self.pos), (dict_humains_prec[self.cible_1].pos))
-            pygame.draw.line(screen, (0, 0, 255), (self.pos), (dict_humains_prec[self.cible_2].pos))
+            pygame.draw.line(screen, (0, 0, 255), (self.pos), (self.cible1.pos))
+            pygame.draw.line(screen, (0, 0, 255), (self.pos), (self.cible2.pos))
             pygame.draw.circle(screen, (0, 255, 0), self.pos, 2)
         else:
             pygame.draw.circle(screen, WHITE, self.pos, 2)
@@ -71,7 +71,7 @@ class Humain:
         a = (humain1.pos[1] - humain2.pos[1])/(humain1.pos[0] - humain2.pos[0])
         return a
     
-    def cible_en_vue(self, dict_humains_prec, index_cible):
+    def cible_en_vue(self, dict_humains, index_cible):
         """
         Vérifie que la personne peut voir ses 2 cibles
 
@@ -80,11 +80,11 @@ class Humain:
         Returns : 1 booléen pour chaque cible
         """
         # Pente droite jusqu'à la cible
-        cible, pos_cible = dict_humains_prec[index_cible]
+        cible = dict_humains[index_cible]
         a = self.calculer_pente(self, cible)
 
         
-        for humain in dict_humains_prec.values():
+        for humain in dict_humains.values():
             # Vérification pour cible 1
             if humain != self and humain != cible :
                 if ((humain.pos[0] >= self.pos[0] and humain.pos[0] <= cible.pos[0]) or (humain.pos[0] <= self.pos[0] and humain.pos[0] >= cible.pos[0])) and ((humain.pos[1] >= self.pos[1] and humain.pos[1] <= cible.pos[1]) or (humain.pos[1] <= self.pos[1] and humain.pos[1] >= cible.pos[1])):
@@ -110,7 +110,7 @@ class Interface:
     def display_humain_list(self):
         """Display list of Humains"""
         y_offset = 10
-        for humain, pos in dict_humains_prec.values():
+        for humain in dict_humains.values():
             text = font.render(f"Humain {humain.id}", True, WHITE)
             screen.blit(text, (10, y_offset))
             y_offset += 30
@@ -118,7 +118,7 @@ class Interface:
     def check_click_on_list(self, mouse_pos):
         """Check if humain in list is clicked"""
         y_offset = 10
-        for humain, pos in dict_humains_prec.values():
+        for humain in dict_humains.values():
             text_rect = pygame.Rect(10, y_offset, 100, 30)
             if text_rect.collidepoint(mouse_pos):
                 return humain.id 
@@ -136,9 +136,9 @@ dico_test["B"].court_chemin_vect()
 """
 
 # La dictionnaire des gens
-
-dict_humains_prec = {}
-dict_humains_suiv = {}
+dict_humains = {}
+dict_pos = {}
+dict_pos_suiv = {}
 selected_humain = None  # This will store the ID of the selected humain
 
 # creation de l'interface
@@ -147,11 +147,12 @@ interface = Interface()
 # Création des gens
 for i in range(20):
     humain = Humain(randint(100,600),randint(100,500), i)
-    dict_humains_prec[i] = [humain, humain.pos]
+    dict_humains[i] = humain
+    dict_pos[i] = humain.pos
 
 # Affecter les 2 cibles à chacun des gens
-for humain in dict_humains_prec.values():
-    dict_humains_temp = dict_humains_prec.copy()
+for humain in dict_humains.values() :
+    dict_humains_temp = dict_humains.copy()
     humain.choisir_cible(dict_humains_temp)
 
 # Boucle principale
@@ -173,12 +174,12 @@ while x:
     # Draw the Humain list
     interface.display_humain_list()
 
-    for humain in dict_humains_prec.values():
+    for humain in dict_humains.values():
         if humain.id == selected_humain:
             humain.afficher(highlight=True)
         else:
             humain.afficher()
         humain.calculer_etat_suivant()
-    dict_humains_prec = dict_humains_suiv
+    dict_pos_prec = dict_pos_suiv
 
     pygame.display.update()
